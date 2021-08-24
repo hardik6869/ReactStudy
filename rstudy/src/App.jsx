83708d122e0191dd59abdf3b24534a86d0a1a8de
Hardik Sorthiya <sorthiyahardik6869@gmail.com>
2021-08-24 10:24:03 +0530

    add Inc and dec

--- rstudy/src/App.jsx
+++ rstudy/src/App.jsx
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import './index.css';
 import Basic  from './Basic';
 import ToDo from './ToDo';
+import IncAndDec from './IncAndDec';
 
 function App (){
-    return(<> <ToDo/> </>
+    return(<> <IncAndDec /> </>
     )
 }
 export default App;
---
+++ rstudy/src/IncAndDec.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
+import Button from '@material-ui/core/Button';
+
+function IncAndDec(){
+    const [num,setNum] = useState(0);
+
+    const clickDec = () => {
+        if (num>0){
+            setNum(num - 1);
+        }else{
+            setNum(0)
+            alert("Plese Increse Your Number")
+        }
+    };
+    const clickInc = () => {
+         setNum(num + 1);
+    };
+    return (
+        <>
+            <div className="main">
+                <div className="Main_box">
+                    <h1> {num} </h1>
+                        <Button onClick={clickInc} className="Inc" > <AddIcon /> </Button>
+                        <Button onClick={clickDec} className="rem"> <RemoveIcon /> </Button>
+                </div>
+            </div>
+        </>
+    )
+    
+}
+export default IncAndDec;
